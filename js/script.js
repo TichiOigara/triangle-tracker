0bbd4abb219a472b5014269c405c005f2956e654
TichiOigara <sheilanyatichi@gmail.com>
2019-02-15 16:04:44 +0300

    add variables to js/script.js.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function triangleTracker(){
-  
+  var a = parseInt(document.getElementById('side1').value);
+  var b = parseInt(document.getElementById('side2').value);
+  var c = parseInt(document.getElementById('side3').value);
+  var triangle;
 }
 //triangleTracker();
