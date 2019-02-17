9e66413dd783817ae7ae33cb3baf4d448e8a2c34
TichiOigara <sheilanyatichi@gmail.com>
2019-02-17 18:00:28 +0300

    add array to js/script.js.

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@ function triangleTracker() {
   var a = parseInt(document.getElementById('side1').value);
   var b = parseInt(document.getElementById('side2').value);
   var c = parseInt(document.getElementById('side3').value);
+  var side =[a,b,c];
   var triangle;
   //control flow that checks if a triangle is isoscles, equilateral, scalene or not a triangle.
-  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  if (isNaN(side[0]) || isNaN(side[1]) || isNaN(side[2])) {
     triangle = "Please enter all three sides of the triangle. Only numerical values are accepted.";
-  } else if (a === 0 || b === 0 || c === 0) {
-    triangle = "O is not a valid entry. Please try again."
+  } else if (side[0] === 0 || side[1] === 0 || side[2] === 0) {
+    triangle = "O(zero) is not a valid entry. Please try again."
   } else {
-    if (a + b > c && b + c > a && c + a > b) {
-      if (a === b && b === c && c === a) {
+    if (side[0] + side[1] > side[2] && side[0] + side[2] > side[1] && side[1] + side[2] > side[0]) {
+      if (side[0] === side[1] && side[1] === side[2] && side[2] === side[0]) {
         triangle = "The triangle is equilateral. All the sides are equal.";
-      } else if (a === b || b === c || c === a) {
+      } else if (side[0] === side[1] || side[1] === side[2] || side[2] === side[0]) {
         triangle = "The triangle is isosceles. Only two out of the three sides are equal.";
       } else {
         triangle = "The triangle is scalene. None of its sides are equal to one another. ";
